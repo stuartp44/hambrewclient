147257d6fd13af7c032cc4f7ecd8843965e4d569
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2026-01-16 19:10:26 +0100

    refactor: remove unnecessary __init__ method from options flow handler

--- custom_components/minibrew/config_flow.py
+++ custom_components/minibrew/config_flow.py
@@ -79,9 +79,6 @@ class PymbrewClientConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class PymbrewClientOptionsFlowHandler(config_entries.OptionsFlow):
     """Handle options flow for PymbrewClient."""
 
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         if user_input is not None:
